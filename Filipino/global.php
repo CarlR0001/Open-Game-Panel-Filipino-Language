1c31d15887e51753adf21fac81f044c352751b8d
CarlR <103570356+CarlR0001@users.noreply.github.com>
2022-07-01 11:57:47 +0800

    Add files via upload

---
+++ Filipino/global.php
@@ -0,0 +1,144 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ * Language Created By: CarlR.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Credits To CarlR. For this Filipino OGP Translation.
+ */
+ 
+define('OGP_LANG_lang_charset', "UTF-8");
+define('OGP_LANG_already_logged_in_redirecting_to_dashboard', "Naka-log in ka na, nagre-redirect sa Dashboard.");
+define('OGP_LANG_logging_in', "Logging in");
+define('OGP_LANG_redirecting_in', "Redirecting in");
+define('OGP_LANG_refresh_page', "i-refresh ang page");
+define('OGP_LANG_no_rights', "Wala kang karapatang i-access ang page na ito.");
+define('OGP_LANG_welcome', "Maligayang pagdating");
+define('OGP_LANG_logout', "Mag-logout");
+define('OGP_LANG_logout_message', "Ikaw ay na-log out.");
+define('OGP_LANG_support', "Support");
+define('OGP_LANG_password', "Password");
+define('OGP_LANG_login', "Username");
+define('OGP_LANG_login_button', "Login");
+define('OGP_LANG_solve_captcha', "Human Check");
+define('OGP_LANG_lost_passwd', "Nakalimutan ang iyong password?");
+define('OGP_LANG_no_db_connection', "Hindi makakonekta sa database.");
+define('OGP_LANG_bad_login', "Di-wastong username o password.");
+define('OGP_LANG_not_logged_in', "Kasalukuyang hindi ka naka-log in.");
+define('OGP_LANG_remove_install', "Mangyaring alisin ang install.php file para sa mga kadahilanang pangseguridad.");
+define('OGP_LANG_agent_offline', "Ang Ahente na kumokontrol sa server na ito ay offline.");
+define('OGP_LANG_logged_in', "Ikaw ay naka-log in bilang");
+define('OGP_LANG_delete', "Burahin");
+define('OGP_LANG_edit', "i-Edit");
+define('OGP_LANG_actions', "Mga Aksyon");
+define('OGP_LANG_invalid_subpage', "Di-wastong subpage.");
+define('OGP_LANG_invalid_home_id', "Di-wastong home ID ang ipinasok.");
+define('OGP_LANG_note', "TANDAAN");
+define('OGP_LANG_hint', "HINT");
+define('OGP_LANG_yes', "Oo");
+define('OGP_LANG_no', "Hindi");
+define('OGP_LANG_on', "On");
+define('OGP_LANG_off', "Off");
+define('OGP_LANG_db_error_invalid_host', "Hindi wastong database host ang ipinasok.");
+define('OGP_LANG_db_error_invalid_user_and_pass', "Maling database username o password.");
+define('OGP_LANG_db_error_invalid_database', "Di-wastong database.");
+define('OGP_LANG_db_unknown_error', "Hindi matukoy na database error: %s");
+define('OGP_LANG_db_error_module_missing', "Nawawala ang kinakailangang PHP database module.");
+define('OGP_LANG_db_error_invalid_db_type', "Di-wastong uri ng database sa config file.");
+define('OGP_LANG_invalid_login_information', "Hindi wastong impormasyon sa pag-log in ang ipinasok.");
+define('OGP_LANG_failed_to_read_config', "Nabigong basahin ang config file.");
+define('OGP_LANG_account_expired', "Nag-expire na ang iyong account.");
+define('OGP_LANG_contact_admin_to_enable_account', "Makipag-ugnayan sa iyong administrator upang muling paganahin ang iyong account.");
+define('OGP_LANG_maintenance_mode_on', "naka-On ang Maintenance mode");
+define('OGP_LANG_logging_out_10', "Magla-log out sa loob ng 10 segundo");
+define('OGP_LANG_invalid_redirect', "Redirecting");
+define('OGP_LANG_login_title', "Pag-login sa Control Panel");
+define('OGP_LANG_module_not_installed', "Hindi naka-install ang module.");
+define('OGP_LANG_no_access_to_home', "Wala kang access sa pahinang ito.");
+define('OGP_LANG_not_available', "N/A");
+define('OGP_LANG_offline', "Offline");
+define('OGP_LANG_online', "Online");
+define('OGP_LANG_invalid_url', "Di-wastong URL");
+define('OGP_LANG_xml_file_not_valid', "Hindi ma-validate ang XML file na '%s' gamit ang schema na '%s'.");
+define('OGP_LANG_unable_to_load_xml', "Hindi ma-load ang XML file na '%s'. Isyu sa pahintulot?");
+define('OGP_LANG_gamemanager', "Tagapamahala ng Laro");
+define('OGP_LANG_game_monitor', "Monitor ng Laro");
+define('OGP_LANG_dashboard', "Dashboard");
+define('OGP_LANG_user_addons', "Addons");
+define('OGP_LANG_ftp', "FTP");
+define('OGP_LANG_shop', "Tindahan");
+define('OGP_LANG_shop_guest', "Tindahan");
+define('OGP_LANG_TS3Admin', "TS3 Admin");
+define('OGP_LANG_administration', "Administrasyon");
+define('OGP_LANG_config_games', "Game/Mod Config");
+define('OGP_LANG_modulemanager', "Mga Modules");
+define('OGP_LANG_server', "Mga Servers");
+define('OGP_LANG_settings', "Mga Setting ng Panel");
+define('OGP_LANG_themes', "Mga setting ng tema");
+define('OGP_LANG_user_admin', "Mga gumagamit");
+define('OGP_LANG_sub_users', "Mga Sub User");
+define('OGP_LANG_show_groups', "Mga Grupo");
+define('OGP_LANG_user_games', "Mga Server ng Laro");
+define('OGP_LANG_addons_manager', "Addons Manager");
+define('OGP_LANG_ftp_admin', "FTP users");
+define('OGP_LANG_orders', "Mga Order");
+define('OGP_LANG_services', "Mga Serbisyo");
+define('OGP_LANG_shop_settings', "Mga Setting ng Shop");
+define('OGP_LANG_update', "I-update ang Panel");
+define('OGP_LANG_extras', "Mga extra");
+define('OGP_LANG_show', "Ipakita");
+define('OGP_LANG_show_all', "Ipakita ang Lahat ng Mga Server");
+define('OGP_LANG_cur_theme', "%s Tema");
+define('OGP_LANG_copyright', "Copyright");
+define('OGP_LANG_all_rights_reserved', "Lahat ng karapatan ay nakalaan");
+define('OGP_LANG_version', "Bersyon");
+define('OGP_LANG_show_version', "Ipakita ang Bersyon");
+define('OGP_LANG_queries_executed', "naisagawang mga tanong");
+define('OGP_LANG_lang', "Wika");
+define('OGP_LANG_get_size', "Ipakita ang Size");
+define('OGP_LANG_total_size', "Kabuuang Sukat");
+define('OGP_LANG_lgsl', "LGSL");
+define('OGP_LANG_lgsl_admin', "LGSL Admin");
+define('OGP_LANG_rcon', "RCON");
+define('OGP_LANG_watch_logger', "Panoorin ang Logger");
+define('OGP_LANG_litefm_settings', "LiteFM Settings");
+define('OGP_LANG_assign_expiration_date', "Magtalaga ng petsa ng pag-expire");
+define('OGP_LANG_assign_expiration_date_info', "Sa sandaling mag-expire na ang server ay hindi nakatalaga ngunit hindi naaalis.");
+define('OGP_LANG_server_expiration_date', "Petsa ng pag-expire ng server");
+define('OGP_LANG_server_expiration_date_info', "Kapag nag-expire na ang server ay aalisin (database at mga file).");
+define('OGP_LANG_set_expiration_date', "Itakda ang petsa ng pag-expire");
+define('OGP_LANG_admin_dsi', "DSi Settings");
+define('OGP_LANG_user_dsi', "DSi");
+define('OGP_LANG_list_dsi', "DSi List");
+define('OGP_LANG_no_remote_servers', "Walang kilalang remote server! Magdagdag ng server para magamit ang function na ito.");
+define('OGP_LANG_no_results_found', "Walang nakitang resulta ng paghahanap para sa %s");
+define('OGP_LANG_tickets', "Mga tiket ng suporta");
+define('OGP_LANG_news', "Balita");
+define('OGP_LANG_admin_news', "Admin ng Balita");
+define('OGP_LANG_util', "Mga utility");
+define('OGP_LANG_fast_download', "Mabilis na Download");
+define('OGP_LANG_fd_user', "Mabilis na Download");
+define('OGP_LANG_cron', "Admin ng Cron");
+define('OGP_LANG_user_cron', "Cron");
+define('OGP_LANG_faq', "F.A.Q.");
+define('OGP_LANG_mysql_admin', "Admin ng MySQL");
+define('OGP_LANG_copied', "Nakopya!");
+define('OGP_LANG_ticket_settings', "Mga Setting ng Ticket");
+define('OGP_LANG_form', "Magrehistro");
+define('OGP_LANG_timed_out', "Nag-time out ang operasyon (ngunit maaari pa ring matagumpay na makumpleto).");
+?>
